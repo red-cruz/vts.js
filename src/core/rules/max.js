59cddbaa1f9b2e2bf49e6477eaaf0e103373498c
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2024-02-24 21:19:44 +0800

    refactor: min and max rule

--- src/core/rules/max.js
+++ src/core/rules/max.js
@@ -1,45 +1,94 @@
 // @ts-check
 import defaultMsg from '../../defaults/defaultMsg';
+import VtsFormValidator from '../../utils/VtsFormValidator';
+import attachEvent from '../../utils/attachEvent';
 
 /**
  * @param {import('../../types/config/rules').Rules[string]} rules
  * @param {HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement} field
  * @param {string} label
  * @this {import('../../types/core/index').default} Vts
- * @returns {import('../../types/core/validation').ValidationResults}
+ * @returns {Promise<import('../../types/core/validation').ValidationResults>}
  */
-export default function maxRule(rules, field, label) {
-  const max = rules.max || Number(field.dataset.vtsRuleMax);
-  if (!max) return {};
+export default async function (rules, field, label) {
+  const maxRule = rules.max;
 
-  const messages = rules.messages?.max || this.messages?.max || defaultMsg.max;
+  if (!maxRule) return {};
 
+  let maxNumber = 0;
   let valid = false;
 
-  if (field instanceof HTMLInputElement) {
-    switch (field.type) {
-      case 'number':
-        valid = Number(field.value) <= max;
+  const getErrMsg = (num = maxNumber) => {
+    const errMsg = {
+      max: rules.messages?.max || this.messages?.max || defaultMsg.max,
+    };
+
+    errMsg.max = errMsg.max.replace(/{:max}/g, String(num));
+
+    return errMsg;
+  };
+
+  try {
+    switch (typeof maxRule) {
+      case 'function':
+        this._setCheckingRule(rules, field, label);
+        const max = await maxRule(field, label);
+
+        if (typeof max === 'string') {
+          let maxSrc = max;
+          if (max.startsWith('field:')) {
+            const targetField = VtsFormValidator.validateField(
+              this.form,
+              max.replace('field:', '')
+            );
+            attachEvent('max', targetField, field, rules);
+            maxSrc = targetField.value;
+          } else maxSrc = max;
+
+          maxNumber = Number(maxSrc);
+        } else maxNumber = max;
         break;
 
-      case 'file':
-        const fileLen = field.files?.length;
-        valid = fileLen === undefined ? false : fileLen <= max;
+      case 'number':
+        maxNumber = maxRule;
         break;
 
       default:
-        valid = field.value.length <= max;
-        break;
+        let maxSrc = maxRule;
+        if (maxRule.startsWith('field:')) {
+          const targetField = VtsFormValidator.validateField(
+            this.form,
+            maxRule.replace('field:', '')
+          );
+          attachEvent('max', targetField, field, rules);
+          maxSrc = targetField.value;
+        } else maxSrc = maxRule;
+
+        maxNumber = Number(maxSrc);
     }
-  } else if (field instanceof HTMLSelectElement) {
-    valid = field.selectedOptions.length <= max;
-  } else {
-    valid = field.value.length <= max;
+
+    if (field instanceof HTMLInputElement) {
+      switch (field.type) {
+        case 'file':
+          const fileLen = field.files?.length;
+          valid = fileLen === undefined ? false : fileLen <= maxNumber;
+          break;
+
+        case 'number':
+          field.max = String(maxNumber);
+        default:
+          valid = Number(field.value) <= maxNumber;
+          break;
+      }
+    } else if (field instanceof HTMLSelectElement) {
+      valid = field.selectedOptions.length <= maxNumber;
+    } else {
+      valid = field.value.length <= maxNumber;
+    }
+  } catch (error) {
+    console.error(error);
+    return getErrMsg(0);
   }
 
-  return valid
-    ? {}
-    : {
-        max: messages.replace(/{:max}/g, String(max)),
-      };
+  return valid ? {} : getErrMsg();
 }
--- src/core/rules/min.js
+++ src/core/rules/min.js
@@ -28,63 +28,66 @@ export default async function (rules, field, label) {
     return errMsg;
   };
 
-  switch (typeof minRule) {
-    case 'function':
-      this._setCheckingRule(rules, field, label);
-      const min = await minRule(field, label);
+  try {
+    switch (typeof minRule) {
+      case 'function':
+        this._setCheckingRule(rules, field, label);
+        const min = await minRule(field, label);
 
-      if (typeof min === 'string') {
-        let minSrc = min;
-        if (min.startsWith('field:')) {
-          const targetField = VtsFormValidator.validateField(
-            this.form,
-            min.replace('field:', '')
-          );
-          attachEvent('min', targetField, field, rules);
-          minSrc = targetField.value;
-        } else minSrc = min;
+        if (typeof min === 'string') {
+          let minSrc = min;
+          if (min.startsWith('field:')) {
+            const targetField = VtsFormValidator.validateField(
+              this.form,
+              min.replace('field:', '')
+            );
+            attachEvent('min', targetField, field, rules);
+            minSrc = targetField.value;
+          } else minSrc = min;
 
-        minNumber = Number(minSrc);
-      } else minNumber = min;
-      break;
-
-    case 'number':
-      minNumber = minRule;
-      break;
-
-    default:
-      let minSrc = minRule;
-      if (minRule.startsWith('field:')) {
-        const targetField = VtsFormValidator.validateField(
-          this.form,
-          minRule.replace('field:', '')
-        );
-        attachEvent('min', targetField, field, rules);
-        minSrc = targetField.value;
-      } else minSrc = minRule;
-
-      minNumber = Number(minSrc);
-  }
-
-  if (field instanceof HTMLInputElement) {
-    switch (field.type) {
-      case 'number':
-        valid = Number(field.value) >= minNumber;
+          minNumber = Number(minSrc);
+        } else minNumber = min;
         break;
 
-      case 'file':
-        const fileLen = field.files?.length;
-        valid = fileLen === undefined ? false : fileLen >= minNumber;
+      case 'number':
+        minNumber = minRule;
         break;
 
       default:
-        valid = field.value.length >= minNumber;
-        break;
+        let minSrc = minRule;
+        if (minRule.startsWith('field:')) {
+          const targetField = VtsFormValidator.validateField(
+            this.form,
+            minRule.replace('field:', '')
+          );
+          attachEvent('min', targetField, field, rules);
+          minSrc = targetField.value;
+        } else minSrc = minRule;
+
+        minNumber = Number(minSrc);
     }
-  } else if (field instanceof HTMLSelectElement) {
-    valid = field.selectedOptions.length >= minNumber;
-  } else {
-    valid = field.value.length >= minNumber;
+
+    if (field instanceof HTMLInputElement) {
+      switch (field.type) {
+        case 'file':
+          const fileLen = field.files?.length;
+          valid = fileLen === undefined ? false : fileLen >= minNumber;
+          break;
+
+        case 'number':
+          field.min = String(minNumber);
+        default:
+          valid = Number(field.value) >= minNumber;
+          break;
+      }
+    } else if (field instanceof HTMLSelectElement) {
+      valid = field.selectedOptions.length >= minNumber;
+    } else {
+      valid = field.value.length >= minNumber;
+    }
+  } catch (error) {
+    console.error(error);
+    return getErrMsg(0);
   }
 
   return valid ? {} : getErrMsg();
