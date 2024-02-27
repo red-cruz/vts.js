4e97ecb675d4ec800609225c7dcdd74135f33cb2
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2024-02-27 17:16:20 +0800

    refactor: max rule

--- src/core/rules/max.js
+++ src/core/rules/max.js
@@ -1,79 +1,59 @@
 // @ts-check
 import defaultMsg from '../../defaults/defaultMsg';
 import getFieldLabel from '../../utils/getFieldLabel';
-import getMinOrMax from '../../utils/rules/getMinOrMax';
+import getRuleValue from '../../utils/rules/getRuleValue';
 
 /**
  * @param {import('../../types/config/rules').Rules[string]} rules
  * @param {import('../../types/core/index').VtsField} field
  * @param {string} label
  * @this {import('../../types/core/index').default} Vts
- * @returns {Promise<import('../../types/core/validation').ValidationResults>}
  */
 export default async function (rules, field, label) {
-  const minRule = rules.max;
+  if (!rules.max) return {};
 
-  if (!minRule) return {};
+  /** @type {{ruleValue: number, targetField: import('../../types/core/index').VtsField|undefined}} */ // @ts-ignore
+  const { ruleValue, targetField } = await getRuleValue(
+    this,
+    rules,
+    field,
+    label,
+    'max'
+  );
 
-  let max = 0;
-  let valid = false;
+  let isValid = false;
 
-  /** @type {import('../../types/core/index').VtsField|undefined} */
-  let targetField;
+  if (field instanceof HTMLInputElement) {
+    switch (field.type) {
+      case 'file':
+        const fileLen = field.files?.length;
+        isValid = fileLen === undefined ? false : fileLen <= ruleValue;
+        break;
 
-  const getErrMsg = (num = max) => {
-    const errMsg = {
-      max: rules.messages?.max || this.messages?.max || defaultMsg.max,
-    };
-
-    errMsg.max = errMsg.max
-      .replace(/{:max}/g, String(num))
-      .replace(/{:label}/g, label);
-
-    if (targetField) {
-      const targetRules = this._getFieldRules(targetField);
-      errMsg.max = errMsg.max
-        .replace(/{:targetValue}/g, targetField.value)
-        .replace(
-          /{:targetLabel}/g,
-          getFieldLabel(targetRules.label, targetField, this.form)
-        );
+      case 'number':
+        field.max = String(ruleValue);
+      default:
+        isValid = Number(field.value) <= ruleValue;
+        break;
     }
-
-    return errMsg;
-  };
-
-  try {
-    /** @type {{max: number, targetField?:import('../../types/core/index').VtsField}} */
-    const awaitedMin = await getMinOrMax.call(this, 'max', rules, field, label);
-    max = awaitedMin.max;
-    targetField = awaitedMin.targetField;
-
-    if (field instanceof HTMLInputElement) {
-      switch (field.type) {
-        case 'file':
-          const fileLen = field.files?.length;
-          valid = fileLen === undefined ? false : fileLen <= max;
-          break;
-
-        case 'checkbox':
-          return getErrMsg();
-
-        case 'number':
-          field.max = String(max);
-        default:
-          valid = Number(field.value) <= max;
-          break;
-      }
-    } else if (field instanceof HTMLSelectElement) {
-      valid = field.selectedOptions.length <= max;
-    } else {
-      valid = field.value.length <= max;
-    }
-  } catch (error) {
-    console.error(error);
-    return getErrMsg(0);
+  } else if (field instanceof HTMLSelectElement) {
+    isValid = field.selectedOptions.length <= ruleValue;
+  } else {
+    isValid = field.value.length <= ruleValue;
   }
 
-  return valid ? {} : getErrMsg();
+  if (isValid) return {};
+
+  const messages = rules.messages?.max || this.messages?.max || defaultMsg.max;
+
+  const targetLabel = targetField
+    ? getFieldLabel(rules.label, targetField, this.form)
+    : '';
+
+  return {
+    max: messages
+      .replace(/{:max}/g, String(ruleValue))
+      .replace(/{:targetValue}/g, targetField?.value ?? '')
+      .replace(/{:targetLabel}/g, targetLabel),
+  };
 }
